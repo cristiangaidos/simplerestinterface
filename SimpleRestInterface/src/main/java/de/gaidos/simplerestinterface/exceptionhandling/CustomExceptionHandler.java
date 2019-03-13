daecfb053776fe1e4c84acf9843a544e01ca7d34
c.gaidos <cristian@gaidos.de>
2019-03-13 11:24:37 +0100

    Added more Exception handling

--- SimpleRestInterface/src/main/java/de/gaidos/simplerestinterface/exceptionhandling/CustomExceptionHandler.java
+++ SimpleRestInterface/src/main/java/de/gaidos/simplerestinterface/exceptionhandling/CustomExceptionHandler.java
@@ -1,5 +1,7 @@
+
 package de.gaidos.simplerestinterface.exceptionhandling;
 
+import java.rmi.ServerException;
 import java.util.Date;
 
 import org.springframework.http.HttpStatus;
@@ -14,17 +16,21 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@ExceptionHandler(PersonNotFoundException.class)
-	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(PersonNotFoundException ex,
-			WebRequest request) {
-		ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-	}
-	
-	@ExceptionHandler(ColorNotFoundException.class)
-	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(ColorNotFoundException ex,
-			WebRequest request) {
-		ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-	}
+  @ExceptionHandler(PersonNotFoundException.class)
+  public final ResponseEntity<ErrorDetails> handleUserNotFoundException(PersonNotFoundException ex, WebRequest request) {
+    ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+    return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(ColorNotFoundException.class)
+  public final ResponseEntity<ErrorDetails> handleUserNotFoundException(ColorNotFoundException ex, WebRequest request) {
+    ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+    return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(ColorNotFoundException.class)
+  public final ResponseEntity<ErrorDetails> handleUserNotFoundException(ServerException ex, WebRequest request) {
+    ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+    return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 }
--- SimpleRestInterface/src/main/java/de/gaidos/simplerestinterface/utils/CSVUtils.java
+++ SimpleRestInterface/src/main/java/de/gaidos/simplerestinterface/utils/CSVUtils.java
@@ -9,39 +9,41 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import de.gaidos.simplerestinterface.exceptionhandling.ServerException;
+
 public class CSVUtils {
 
-	public static final String DEFAULT_SEPARATOR = ",";
+  public static final String DEFAULT_SEPARATOR = ",";
 
-	public static void writeCSVFile(String csvFilePath, String line) {
-		try {
-			FileWriter pw = new FileWriter(csvFilePath, true);
-			pw.append("\n");
-			pw.append(line);
-			pw.append("\n");
-            pw.flush();
-            pw.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+  public static void writeCSVFile(String csvFilePath, String line) {
+    try {
+      FileWriter pw = new FileWriter(csvFilePath, true);
+      pw.append("\n");
+      pw.append(line);
+      pw.append("\n");
+      pw.flush();
+      pw.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new ServerException(e.getMessage());
+    }
+  }
 
-	public static List<String> readCSVFile(String csvFilePath) {
-		List<String> lines = new ArrayList<String>();
-		try {
-			Scanner scanner = new Scanner(new File(csvFilePath));
-			while (scanner.hasNext()) {
-				String nextLine = scanner.nextLine();
-				if (nextLine != null && !nextLine.trim().isEmpty()) {
-					lines.add(nextLine);
-				}
-			}
-			scanner.close();
-		} catch (FileNotFoundException e) {
-			// TODO: handle exception
-		}
-		return lines;
-	}
+  public static List<String> readCSVFile(String csvFilePath) {
+    List<String> lines = new ArrayList<String>();
+    try {
+      Scanner scanner = new Scanner(new File(csvFilePath));
+      while (scanner.hasNext()) {
+        String nextLine = scanner.nextLine();
+        if (nextLine != null && !nextLine.trim().isEmpty()) {
+          lines.add(nextLine);
+        }
+      }
+      scanner.close();
+    } catch (FileNotFoundException e) {
+      throw new ServerException(e.getMessage());
+    }
+    return lines;
+  }
 
 }
